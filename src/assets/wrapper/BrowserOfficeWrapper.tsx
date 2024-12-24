0360c76c09c7f78138a391e38725b4ace0629e24
kharismatara <kharismataram@gmail.com>
2024-12-24 09:48:13 +0700

    api office

--- src/assets/wrapper/BrowserOfficeWrapper.tsx
+++ src/assets/wrapper/BrowserOfficeWrapper.tsx
@@ -1,5 +1,44 @@
+import { useEffect, useState } from "react";
 import OfficeCard from "../component/OfficeCard";
+import { Office } from "../types/type";
+import axios from "axios";
 function BrowserOfficeWrapper() {
+  const [offices, setOffices] = useState<Office[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    axios
+      .get("http://127.0.0.1:8000/api/offices", {
+        headers: {
+          "X-API-KEY": "sdfghjk345678",
+        },
+      })
+      .then((response) => {
+        console.log(response.data); // Log the response to check if the data exists
+        setOffices(response.data.data); // Ensure 'data' is an array
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error(error); // Log the error
+        setError("An error occurred while loading cities.");
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <p>Loading ...</p>;
+  }
+
+  if (error) {
+    return <p>Error loading data: {error}</p>;
+  }
+
+  // Handle empty data scenario
+  if (offices.length === 0) {
+    return <p>No cities available.</p>;
+  }
+
   return (
     <>
       <section id="Fresh-Space" className="flex flex-col gap-[30px] w-full max-w-[1130px] mx-auto mt-[100px] mb-[120px]">
@@ -9,7 +48,9 @@ function BrowserOfficeWrapper() {
           For Your Better Productivity.
         </h2>
         <div className="grid grid-cols-3 gap-[30px]">
-          <OfficeCard></OfficeCard>
+          {offices.map((office) => (
+            <OfficeCard key={office.id} office={office}></OfficeCard>
+          ))}
         </div>
       </section>
     </>
